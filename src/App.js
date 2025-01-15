828737e8e71c7400ddb7e182e2aa0bb3cc468f2a
Rakshit <rakshitsaxena96@gmail.com>
2025-01-15 18:18:04 +0530

    News->react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/*This project----->To understand class based component*/
+import './App.css';
+import React, { Component } from 'react'
+import Navbar from './Component/Navbar'
+import News from './Component/News' 
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+       <Navbar/>
+       <News/>
+      </div>
+    )
+  }
+}
---
+++ src/Component/News.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is a news component
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/>
+        <Newsitem/> 
+        <Newsitem/>
+        
+      </div>
+    )
+  }
+}
+
+export default News
